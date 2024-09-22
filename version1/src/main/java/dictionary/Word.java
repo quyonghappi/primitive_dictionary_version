6503b4d12645db4d2441e58c4ed321abcc305769
manh <163654667+quyonghappi@users.noreply.github.com>
2024-09-22 21:29:47 +0700

    Update Word.java

--- version1/src/main/java/dictionary/Word.java
+++ version1/src/main/java/dictionary/Word.java
@@ -1,8 +1,18 @@
 package dictionary;
 
 public class Word {
-    public String word_target;
+    public String word;
     private String word_explain;
+
+    //constructor
+    public Word(String word, String word_explain) {
+        this.word = word;
+        this.word_explain = word_explain;
+    }
+
+    public String getWord() {
+        return word;
+    }
 
     public String getWord_explain() {
         return word_explain;
@@ -10,5 +20,9 @@ public class Word {
 
     public void setWord_explain(String word_explain) {
         this.word_explain = word_explain;
+    }
+
+    public String toString() {
+        return word + "\t" + word_explain;
     }
 }
